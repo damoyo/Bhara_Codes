e240e3b4ea7a5689e5cd66a42d7ddf65d1d775e1
KXNGZ-W <mkmoyot@gmail.com>
2020-10-13 13:52:54 +0800

    almost done

---
+++ house.py
@@ -0,0 +1,25 @@
+import rat
+
+class House:
+    def __init__(self, colour, bedrooms): #custom constructor
+        self.name = bedrooms
+        self.colour = colour
+        self.ratlist = []
+
+    def number_rats(self):
+        return (len(self.ratlist))
+
+    def add_rat(self, rat):
+        self.ratlist.append(rat)
+
+
+
+rat1 = rat.Rat("Black", 12)
+rat2 = rat.Rat("White", 10)
+#rat1.make_noise()
+#rat2.make_noise()
+House1 = House("Black", 3)
+House2 = House("White", 2)
+House1.add_rat(rat1)
+House1.add_rat(rat2)
+print(House1.number_rats())
---
+++ rat.py
@@ -0,0 +1,9 @@
+class Rat:
+    def __init__(self,  colour, height): #custom constructor
+        self.height = height
+        self.colour = colour
+
+    def make_noise(self):
+        print("Squeak Squeak")
+
+
